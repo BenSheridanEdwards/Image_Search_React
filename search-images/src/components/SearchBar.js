eac2c746d1095706048c74c06cff8f99bd9650f5
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-01 14:43:05 +0100

    Change App.js into a class-based component and use props to pass the search term from parent to child

--- search-images/src/components/SearchBar.js
+++ search-images/src/components/SearchBar.js
@@ -3,10 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.term)
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
