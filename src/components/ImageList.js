6381f0691659d30416a6aecbdadbf2cdc9331651
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-15 13:13:58 +0100

    test: fully test ImageList component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -7,7 +7,7 @@ const ImageList = (props) => {
     return <ImageCard key={image.id} image={image} />
   });
 
-  return <div className="image-list">{images}</div>;
+  return <div className="image-list" data-test="image">{images}</div>;
 };
 
 export default ImageList;
---
+++ src/components/ImageList.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { checkProps, findByTestAttr } from '../test/testUtils';
+
+import ImageList from './ImageList';
+
+const defaultProps = {
+  images: [{ id: "image" }]
+}
+
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<ImageList { ...setupProps } />)
+}
+
+it('does not throw warning with expected props', () => {
+  checkProps(ImageList, defaultProps)
+})
+
+describe('When the user searches and is returned a list of images', () => {
+  let wrapper 
+  beforeEach(() => {
+    wrapper = setup([{ id: "image.jpg" }])
+  })
+
+  it('renders component without error', () => {
+    const component = findByTestAttr(wrapper, 'image')
+    expect(component.length).toBe(1)
+  })
+
+  it('renders an image card', () => {
+    const imageList = findByTestAttr(wrapper, 'image')
+    expect(imageList.text()).toBe("<ImageCard />")
+  })
+})
