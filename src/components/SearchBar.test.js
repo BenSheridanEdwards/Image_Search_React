d4daf738856f27844017e41b1d8b761ff8a2931c
bensheridanedwards <bensheridanedwards@gmail.com>
2020-04-28 00:05:33 +0100

    test: search bar component rendering and onFormSumit and onInputChange methods

--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { checkProps, findByTestAttr } from '../test/testUtils';
+import { shallow, mount } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 
 import SearchBar from './SearchBar';
 
@@ -10,8 +10,30 @@ const setup = (props={}, state=null) => {
     return wrapper;
 }
 
-test('renders without error', () => {
-  const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, 'component-search-bar');
-  expect(appComponent.length).toBe(1);
+it("renders the SearchBar component", () =>{
+  const wrapper = setup({}, { term: '' });
+  const searchBarComponent = findByTestAttr(wrapper, 'component-search-bar');
+  expect(searchBarComponent.length).toBe(1);
+});
+
+it('should call onFormSubmit when a search term is submitted', () => {
+  const mockFunction = jest.fn();
+
+  const component = mount(
+    <SearchBar onFormSubmit={mockFunction} />
+  );
+  component.find('form').simulate('submit');
+  expect(mockFunction).toHaveBeenCalled();
+  
+  component.unmount();
+});
+
+it("should change the update the state of 'term' when the input is changed", () => {
+  const component = mount(<SearchBar />);
+
+  expect(component.state()).toEqual({ term: "" })
+  component.find('input').simulate('change', { target: { name: 'term', value: "test" }});
+  expect(component.state()).toEqual({ term: "test" })
+
+  component.unmount();
 });
